bcee376f04f236897d7a6ccfdaa2c5fcb0d64e44
Daniel Cardoso <danielcardosods@gmail.com>
2013-08-06 16:29:29 -0300

    Add more test case for Wikness analysesTheory method

--- test/witness.test.js
+++ test/witness.test.js
@@ -12,9 +12,17 @@ describe( 'Witness', function() {
     var witness = new Witness( crime );
 
     describe( 'analysesTheory()', function() {
-        var theory = [ 2, 4, 3 ];
         it( 'should return 0', function() {
-            assert.equal( 0, witness.analysesTheory( theory ) );
+            assert.equal( 0, witness.analysesTheory( [ 2, 4, 3 ] ) );
+        });
+        it( 'should return 1', function() {
+            assert.equal( 1, witness.analysesTheory( [ 1, 4, 3 ] ) );
+        });
+        it( 'should return 2', function() {
+            assert.equal( 2, witness.analysesTheory( [ 2, 1, 3 ] ) );
+        });
+        it( 'should return 3', function() {
+            assert.equal( 3, witness.analysesTheory( [ 2, 4, 1 ] ) );
         });
     });
 });
