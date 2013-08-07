152674e9fea5f781cd0c797d3ce1838179cb230c
Daniel Cardoso <danielcardosods@gmail.com>
2013-08-07 00:49:30 -0300

    Add main program with basic output

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+var Crime = require('./crime');
+var Detective = require('./detective');
+var Witness = require('./witness');
+
+var crime = new Crime(
+    JSON.parse( fs.readFileSync( __dirname + '/../data/data.json', 'utf8' ) ),
+    JSON.parse( fs.readFileSync( __dirname + '/../data/response.json', 'utf8' ) )
+);
+var detective = new Detective( crime );
+var witness = new Witness( crime );
+
+var theory;
+var result;
+do {
+    theory = detective.providesTheory();
+    console.log( theory );
+
+    result = detective.processWitnessAnswer(  witness.analysesTheory( theory ) );
+    console.log( result );
+
+} while ( result !== 0 );
